a34e26125fe2f293236a8e66a0ced64ce6e33115
CaiCai <hi@cai-cai.me>
2016-01-23 07:09:55 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,66 @@
+$(function() {
+
+  CMS.init({
+
+    // Name of your site or location of logo file ,relative to root directory (img/logo.png)
+    siteName: 'My sss',
+
+    // Tagline for your site
+    siteTagline: 'Your sss tagline',
+
+    // Email address
+    siteEmail: 'your_email@example.com',
+
+    // Name
+    siteAuthor: 'Your Name',
+
+    // Navigation items
+    siteNavItems: [
+      { name: 'Github', href: 'https://github.com/hi-caicai', newWindow: false},
+      { name: 'About'}
+    ],
+
+    // Posts folder name
+    postsFolder: 'posts',
+
+    // Homepage posts snippet length
+    postSnippetLength: 120,
+
+    // Pages folder name
+          pagesFolder: 'pages',
+
+          // Order of sorting (true for newest to oldest)
+          sortDateOrder: true,
+
+    // Site fade speed
+    fadeSpeed: 300,
+
+    // Site footer text
+    footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+    // Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
+    // to Github
+    mode: 'Github',
+
+    // If Github mode is set, your Github username and repo name. Defaults
+    // to Github pages branch (gh-pages)
+    githubUserSettings: {
+      username: 'yourusername',
+      repo: 'yourrepo'
+    }
+
+  });
+
+  // Markdown settings
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+  });
+
+});
